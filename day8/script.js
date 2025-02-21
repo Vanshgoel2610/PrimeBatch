1731caf593231020ff3ecb7fd0faa71421aa8636
VanshGoel <goelvansh770@gmail.com>
2025-02-21 15:40:22 +0530

    now select and delete can work altogether

--- day8/script.js
+++ day8/script.js
@@ -1,41 +1,51 @@
 const data = [
     {
-        id: 1,
+        id: '1',
         name: "Rakesh",
         city: "Delhi"
     },
     {
-        id: 2,
+        id: '2',
         name: "Mohan",
         city: "Noida"
     },
     {
-        id: 3,
+        id: '3',
         name: "Rishabh",
         city: "Mumbai"
-    }
+    },
+    {
+        id: '4',
+        name: "Raj",
+        city: "Delhi"
+    },
 ];
 
 const root = document.getElementById('root')
 
 const showCards = (newData) => {
     root.innerHTML = ""
-    newData.forEach((ele) => {
+    newData.forEach((ele, idx) => {
         const card = document.createElement('div');
         card.className = "card"
         card.innerHTML = `
         <div>
             <h1>${ele.name}</h1>
             <p>${ele.city}</p>
-            <button onClick = "deleteCard(event)">Delete</button>
+            <button onClick = "deleteCard(event, ${idx})">Delete</button>
 
         </div>
         `
         root.appendChild(card)
     })
 }
-const deleteCard = (event) => {
-    event.target.parentElement.parentElement.remove()
+const deleteCard = (event, idx) => {
+    // One way to delete
+    // event.target.parentElement.parentElement.remove()
+
+    // Another way to delete
+    data.splice(idx, 1)
+    showCards(data)
 }
 const handleSelect = (e) => {
     const selectedCity = e.target.value;
